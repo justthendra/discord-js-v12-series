80fdef08c917b4be52dc2c4cfc19492a189c616c
Thendra <hasan.erdem211@gmail.com>
2021-11-28 04:03:31 +0300

    Update unban.js

--- ban-kick-sistemi/unban.js
+++ ban-kick-sistemi/unban.js
@@ -8,7 +8,7 @@ exports.run = async (client, message, args) => {
     const banlog = message.guild.channels.cache.get(kanal)
     const kisi = message.mentions.members.first() || client.users.resolve(args[0]) || client.users.cache.find(u => u.username === args[0]) || client.users.cache.get(args[0]); 
     if (!kisi) message.reply(`:x: **Yanlış kullanım! Yasağını kaldıracağım kişiyi etiketlemen gerek.**`)
-    const orospu = client.users.cache.get(kisi.id)
+    const unbanab = client.users.cache.get(kisi.id)
     var sebeb = args.slice(1).join(' ') ? args.slice(1).join(' ') : "Sebep Belirtilmemiş"
 
 
@@ -16,7 +16,7 @@ exports.run = async (client, message, args) => {
 
     const banembed = new Discord.MessageEmbed()
     .setTitle(`İşlem - Kullanıcının Yasağını Kaldırma`)
-    .setThumbnail(orospu.displayAvatarURL({ dynamic: true }))
+    .setThumbnail(unbanab.displayAvatarURL({ dynamic: true }))
     .setDescription(`**${kisi}** İsimli kullanıcının yasağı kaldırıldı.\n**Yetkili:** ${message.author}`)
     .setColor(`GREEN`)
     .setFooter(`© 2021 Akhyls`, client.user.avatarURL())
