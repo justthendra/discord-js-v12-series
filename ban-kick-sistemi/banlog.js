9b429e3c40a4b79cb9fdf81594e94f097d11036d
Thendra <hasan.erdem211@gmail.com>
2021-11-28 23:40:33 +0300

    Add files via upload

---
+++ ban-kick-sistemi/banlog.js
@@ -0,0 +1,42 @@
+const Discord = require('discord.js')
+const db = require('quick.db')
+
+exports.run = (client, message, args) => {
+
+    if(args[0] == "ayarla") {
+    const kanal = message.mentions.channels.first()
+    if(!kanal) return message.reply(`:x: **Yanlış Kullanım! Örnek: a-banlog ayarla #kanal**`)
+    db.set(`ThdBanLog_${message.guild.id}`, kanal.id)
+
+    const embed = new Discord.MessageEmbed()
+    .setAuthor(`Akhyls - Ban-Log Sistemi`)
+    .setDescription(`<:onay:894244398940954654> Ban-Log Kanalı başarıyla ${kanal} olarak ayarlandı!`)
+    .setColor('BLACK')
+    .setFooter(`© 2021 Akhyls`, client.user.avatarURL())
+    .setTimestamp()
+    message.channel.send(embed)
+    }
+
+    if(args[0] == "sıfırla") {
+
+    db.delete(`ThdBanLog_${message.guild.id}`)
+    const embed = new Discord.MessageEmbed()
+    .setAuthor(`Akhyls - Ban-Log Sistemi`)
+    .setDescription(`<:onay:894244398940954654> Ban-Log Kanalı başarıyla sıfırlandı!`)
+    .setColor('BLACK')
+    .setFooter(`© 2021 Akhyls`, client.user.avatarURL())
+    .setTimestamp()
+    message.channel.send(embed)
+    }
+}
+
+exports.conf = {
+    permLevel: 2,
+    aliases: []
+}
+
+exports.help = {
+    name: "banlog",
+    description: "botun pingini gösterir",
+    usage: "banlog"
+}
