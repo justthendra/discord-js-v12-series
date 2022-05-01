b74f72491f84846352068311670f0879c58be929
Thendra <hasan.erdem211@gmail.com>
2022-05-01 03:56:24 +0300

    Update kayıt.js

--- kayıt-sistemi/kayıt.js
+++ kayıt-sistemi/kayıt.js
@@ -7,21 +7,21 @@ exports.run = async (client, message, args) => {
 
     const kullanıcı = message.mentions.members.first()
     const member = message.guild.member(kullanıcı)
-    const kanal = db.fetch(`ThdKayıtKanal${message.guild.id}`)
+    const kanal = db.fetch(`CwlKayıtKanal${message.guild.id}`)
 
     const kanals = message.guild.channels.cache.get(kanal)
 
-    const kayıtlırol = db.fetch(`ThdKayıtlıRol${message.guild.id}`)
-    const kayıtsızrol = db.fetch(`ThdKayıtsızRol${message.guild.id}`)
+    const kayıtlırol = db.fetch(`CwlKayıtlıRol${message.guild.id}`)
+    const kayıtsızrol = db.fetch(`CwlKayıtsızRol${message.guild.id}`)
 
     if(!kullanıcı) return message.channel.send("❌ Bir Kullanıcıyı Etiketlemeyi Unuttun!")
     member.roles.add(kayıtlırol)
     member.roles.remove(kayıtsızrol)
     const kayıtbaşarılı = new Discord.MessageEmbed()
-    .setAuthor("ThendraDevs - Kayıt Sistemi")
+    .setAuthor("Curwels - Kayıt Sistemi")
     .setDescription(`<:onay:894244398940954654> ${member} İsimli Kullanıcı Başarıyla Kayıt Edildi. Sunucuya Hoşgeldin ${member}`)
     .setColor("GREEN")
-    .setFooter(`© 2021 Thendra - Kayıt Sistemi`, client.user.displayAvatarURL())
+    .setFooter(`© 2021 Curwels - Kayıt Sistemi`, client.user.displayAvatarURL())
     .setTimestamp()
     kanals.send(kayıtbaşarılı)
     
