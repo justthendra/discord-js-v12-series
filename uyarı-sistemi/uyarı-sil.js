462f96ef7b02e870a783bc14a3f9bfe72e3ebd2a
Thendra <hasan.erdem211@gmail.com>
2022-07-22 23:28:01 +0300

    Update uyarı-sil.js

--- uyarı-sistemi/uyarı-sil.js
+++ uyarı-sistemi/uyarı-sil.js
@@ -5,15 +5,15 @@ exports.run = async (client, message) => {
 
     let user = message.mentions.members.first()  // Uyarısı Silinecek Kişi
     message.guild.members.cache.forEach(user => {
-        db.delete(`CwlUyarıKullanıcı_${message.guild.id}_${user.user.id}`)
+        db.delete(`ThdUyarıKullanıcı_${message.guild.id}_${user.user.id}`)
     })
     
 
     let embed = new Discord.MessageEmbed()
-        .setAuthor(`Curwels - Uyarı Sistemi`)
+        .setAuthor(`Thendra - Uyarı Sistemi`)
         .addField("Uyarı Sıfırlandı", `\n**${message.author}** İsimli Yetkili **${user}** İsimli Kullanıcının Uyarılarını Sıfırladı!`)
         .setColor(`WHITE`)
-        .setFooter("© 2021 Curwels", client.user.avatarURL())
+        .setFooter("© 2021 Thendra", client.user.avatarURL())
         .setTimestamp()
         message.channel.send(embed)
 };
