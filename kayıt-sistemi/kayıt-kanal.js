3aff13db759c32ef12f7c4fe3a22f57dbd00a02a
Thendra <hasan.erdem211@gmail.com>
2024-05-05 13:34:15 +0300

    Update kayıt-kanal.js

--- kayıt-sistemi/kayıt-kanal.js
+++ kayıt-sistemi/kayıt-kanal.js
@@ -2,25 +2,25 @@ const Discord = require('discord.js')
 const db = require('quick.db')
 
 exports.run = async (client, message, args) => {
-    const kanal = message.mentions.channels.first()
-    if(!kanal) return message.channel.send("❌ Bir kanal etiketlemeyi unuttun! Doğru kullanım: `t!kayıt-kanal #kanal`")
+    const channel = message.mentions.channels.first()
+    if(!channel) return message.channel.send("❌ Bir kanal etiketlemeyi unuttun! Doğru kullanım: `t!kayıt-kanal #kanal`")
 
     db.set(`ThdKayıtKanal${message.guild.id}`, kanal.id)
-    let kanalayarla = new Discord.MessageEmbed()
+    let setChannel = new Discord.MessageEmbed()
     .setTitle(`<:onay:894244398940954654> Kayıt Kanalı Ayarlandı`)
-    .setDescription(`<:onay:894244398940954654> Kayıt Kanalı Başarıyla ${kanal} Olarak Ayarlandı.\nSıfırlamak İstiyorsanız \`t!kayıt-kanal sıfırla\` Yazmanız Yeterlidir Olacaktır.`)
+    .setDescription(`<:onay:894244398940954654> Kayıt Kanalı Başarıyla ${channel} Olarak Ayarlandı.\nSıfırlamak İstiyorsanız \`t!kayıt-kanal sıfırla\` Yazmanız Yeterlidir Olacaktır.`)
     .setColor('GREEN')
     .setFooter(`© 2021 Thendra - Kayıt Sistemi`, client.user.displayAvatarURL())
-    message.channel.send(kanalayarla)
+    message.channel.send(setChannel)
 
     if(args[0] === "sıfırla") {
         db.delete(`ThdKayıtKanal${message.guild.id}`)
-        let kanalayarla = new Discord.MessageEmbed()
+        let resChannel = new Discord.MessageEmbed()
         .setTitle(`<:onay:894244398940954654> Kayıt Kanalı Sıfırlandı`)
         .setDescription(`<:onay:894244398940954654> Kayıt Kanalı Başarıyla Sıfırlandı.\nAyarlamak İstiyorsanız \`t!kayıt-kanal #kanal\` Yazmanız Yeterlidir Olacaktır.`)
         .setColor('GREEN')
         .setFooter(`© 2021 Thendra - Kayıt Sistemi`, client.user.displayAvatarURL())
-        message.channel.send(kanalayarla)
+        message.channel.send(resChannel)
     }
 }
 
