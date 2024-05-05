d5753e686f18f8f304bcdd76b33fc4f199ab0111
Thendra <hasan.erdem211@gmail.com>
2024-05-05 13:30:19 +0300

    Update mute-log.js

--- mute-sistemi/mute-log.js
+++ mute-sistemi/mute-log.js
@@ -4,9 +4,9 @@ const db = require('quick.db')
 exports.run = (client, message, args) => {
 
     if(args[0] == "ayarla") {
-    const kanal = message.mentions.channels.first()
-    if(!kanal) return message.reply(`:x: **Yanlış Kullanım! Örnek: a-mutelog ayarla #kanal**`)
-    db.set(`ThdMuteLog_${message.guild.id}`, kanal.id)
+    const channel = message.mentions.channels.first()
+    if(!channel) return message.reply(`:x: **Yanlış Kullanım! Örnek: a-mutelog ayarla #kanal**`)
+    db.set(`ThdMuteLog_${message.guild.id}`, channel.id)
 
     const embed = new Discord.MessageEmbed()
     .setAuthor(`Akhyls - Mute-Log Sistemi`)
