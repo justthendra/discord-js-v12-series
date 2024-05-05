aaf79b0e9339e5b01e844c7e63e975a9fb1a6aa8
Thendra <hasan.erdem211@gmail.com>
2024-05-05 13:29:54 +0300

    Update otorol.js

--- otorol-sistemi/otorol.js
+++ otorol-sistemi/otorol.js
@@ -4,63 +4,63 @@ const db = require('quick.db')
 exports.run = async (client, message, args) => {
 
     if(args[0] === "rol-ayarla") {
-        const rol = message.mentions.roles.first()
-        if(!rol) return message.reply(`:x: **Bir rol etiketlemeyi unuttun! Doğru kullanım: \`a-otorol rol-ayarla @rol\`**`)
-        db.set(`ThdOtorol_${message.guild.id}`, rol.id)
-        let rolayarla = new Discord.MessageEmbed()
+        const role = message.mentions.roles.first()
+        if(!role) return message.reply(`:x: **Bir rol etiketlemeyi unuttun! Doğru kullanım: \`a-otorol rol-ayarla @rol\`**`)
+        db.set(`ThdOtorol_${message.guild.id}`, role.id)
+        let setRole = new Discord.MessageEmbed()
         .setTitle(`Akhyls - Otorol Sistemi`)
-        .setDescription(`<:onay:894244398940954654> Otorol Rolü Başarıyla ${rol} Olarak Ayarlandı.\nSıfırlamak İstiyorsanız \`a-otorol rol-sıfırla\` Yazmanız Yeterlidir Olacaktır.`)
+        .setDescription(`<:onay:894244398940954654> Otorol Rolü Başarıyla ${role} Olarak Ayarlandı.\nSıfırlamak İstiyorsanız \`a-otorol rol-sıfırla\` Yazmanız Yeterlidir Olacaktır.`)
         .setColor('BLACK')
         .setFooter(`© 2021 Akhyls - Otorol Sistemi`, client.user.displayAvatarURL())
-        message.channel.send(rolayarla) 
+        message.channel.send(setRole) 
 
     }
 
     if(args[0] === "rol-sıfırla") {
         db.delete(`ThdOtorol_${message.guild.id}`)
-        let rolsıfırla = new Discord.MessageEmbed()
+        let resRole = new Discord.MessageEmbed()
         .setTitle(`Akhyls - Otorol Sistemi`)
         .setDescription(`<:onay:894244398940954654> Otorol Rolü Başarıyla Sıfırlandı.\nTekrardan Ayarlamak İstiyorsanız \`a-otorol rol-ayarla @rol-ismi\` Yazmanız Yeterlidir Olacaktır.`)
         .setColor('BLACK')
         .setFooter(`© 2021 Akhyls - Otorol Sistemi`, client.user.displayAvatarURL())
-        message.channel.send(rolsıfırla)
+        message.channel.send(resRole)
     }
 
     if(args[0] === "kanal-ayarla") {
-        const kanal = message.mentions.channels.first()
-        if(!kanal) return message.reply(`:x: **Kanal etiketlemeyi unuttun! Doğru kullanım: \`a-otorol kanal-ayarla #kanal\`**`)
-        db.set(`ThdOtorolKanal_${message.guild.id}`, kanal.id)
-        let kanalmsj = new Discord.MessageEmbed()
+        const channel = message.mentions.channels.first()
+        if(!channel) return message.reply(`:x: **Kanal etiketlemeyi unuttun! Doğru kullanım: \`a-otorol kanal-ayarla #kanal\`**`)
+        db.set(`ThdOtorolKanal_${message.guild.id}`, channel.id)
+        let channelEmb = new Discord.MessageEmbed()
         .setTitle(`Akhyls - Otorol Kanal Sistemi`)
-        .setDescription(`<:onay:894244398940954654> Otorol Kanalı Başarıyla ${kanal} Olarak Ayarlandı.\nSıfırlamak İstiyorsanız \`a-otorol kanal-sıfırla\` Yazmanız Yeterlidir Olacaktır.`)
+        .setDescription(`<:onay:894244398940954654> Otorol Kanalı Başarıyla ${channel} Olarak Ayarlandı.\nSıfırlamak İstiyorsanız \`a-otorol kanal-sıfırla\` Yazmanız Yeterlidir Olacaktır.`)
         .setColor('BLACK')
         .setFooter(`© 2021 Akhyls - Otorol Sistemi`, client.user.displayAvatarURL())
-        message.channel.send(kanalmsj)
+        message.channel.send(channelEmb)
     }
 
     if(args[0] === "kanal-sıfırla") {
         db.delete(`ThdOtorolKanal_${message.guild.id}`)
-        let kanalsıfırla = new Discord.MessageEmbed()
+        let resChannel = new Discord.MessageEmbed()
         .setTitle(`Akhyls - Otorol Kanal Sistemi`)
         .setDescription(`<:onay:894244398940954654> Otorol Kanalı Başarıyla Sıfırlandı.\nTekrardan Ayarlamak İstiyorsanız \`a-otorol kanal-ayarla #kanal-ismi\` Yazmanız Yeterlidir Olacaktır.`)
         .setColor('BLACK')
         .setFooter(`© 2021 Akhyls - Otorol Sistemi`, client.user.displayAvatarURL())
-        message.channel.send(kanalsıfırla)
+        message.channel.send(resChannel)
     }
 
     if(args[0] === "sıfırla") {
         db.delete(`ThdOtorolKanal_${message.guild.id}`)
         db.delete(`ThdOtorol_${message.guild.id}`)
-        let sıfırla = new Discord.MessageEmbed()
+        let reset = new Discord.MessageEmbed()
         .setTitle(`Akhyls - Otorol Kanal Sistemi`)
         .setDescription(`<:onay:894244398940954654> Otorol Sistemi Başarıyla Sıfırlandı.\nTekrardan Ayarlamak İstiyorsanız \`a-otorol rol-ayarla @rol-ismi\` Yazmanız Yeterlidir Olacaktır.`)
         .setColor('BLACK')
         .setFooter(`© 2021 Akhyls - Otorol Sistemi`, client.user.displayAvatarURL())
-        message.channel.send(sıfırla)
+        message.channel.send(reset)
     }
 
     if (args[0] == 'yardım') {
-    let yardım = new Discord.MessageEmbed()
+    let help = new Discord.MessageEmbed()
     .setTitle(`Akhyls - Otorol Yardım`)
     .addField('**Otorol Yardım**',' Otorol Komutları Aşağıda Bulunmaktadır.')
     .addField('`a-otorol rol-ayarla`', `Otorol'de verilecek rolü ayarlarsınız.`)
@@ -70,7 +70,7 @@ exports.run = async (client, message, args) => {
     .addField('`a-otorol sıfırla`', `Otorol sistemini sıfırlar.`)
 	.setFooter(`© 2021 Akhyls - Otorol Sistemi`, client.user.displayAvatarURL())
 	.setColor('BLACK')
-	message.channel.send(yardım)
+	message.channel.send(help)
   }
 
 }
