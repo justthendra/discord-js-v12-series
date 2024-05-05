5f81d3a98b9835087d092e612b7d025d43cabd60
Thendra <hasan.erdem211@gmail.com>
2024-05-05 13:15:13 +0300

    Update toplantı-rol.js

--- toplantı-sistemi/toplantı-rol.js
+++ toplantı-sistemi/toplantı-rol.js
@@ -10,40 +10,40 @@ exports.run = async(client, message, args) => {
 
 
     if (!message.member.hasPermission("ADMINISTRATOR")) {
-      const yetkinyokmeh = new discord.MessageEmbed()
+      const notAuthority = new discord.MessageEmbed()
         .setAuthor("❌ Yetersiz Yetki!")
         .setDescription("**Bu Komudu Kullanman İçin `Yönetici` Yetkisine Sahip Olmalısın!**")
         .setColor("RED")
-      return message.channel.send(yetkinyokmeh)
+      return message.channel.send(notAuthority)
     }
 
 
     if (!rolemention) {
-      const mentionyokmeh = new discord.MessageEmbed()
+      const notmention = new discord.MessageEmbed()
         .setAuthor("❌ Eksik Argüman!")
         .setDescription("**Toplantı Rolünü Seçmek İçin Bir Rolü Etiketlemelisiniz!**")
         .setColor("RED")
-      return message.channel.send(mentionyokmeh)
+      return message.channel.send(notmention)
     }
-    const rolid = rolemention.id
-    const sahip = message.guild.members.cache.filter(member => member.roles.cache.find(role => role == rolid))
-    if (sahip.length == 0) {
-      const kimseyokmeh = new discord.MessageEmbed()
+    const roleid = rolemention.id
+    const owner = message.guild.members.cache.filter(member => member.roles.cache.find(role => role == rolid))
+    if (owner.length == 0) {
+      const notRoleow = new discord.MessageEmbed()
         .setAuthor("❌ Yanlış Argüman!")
         .setDescription("**Bu Role Sahip Hiçbir Üye Bu Sunucuda Yok!**")
         .setColor("RED")
-      return message.channel.send(kimseyokmeh)
+      return message.channel.send(notRoleow)
     }
 
-    sahip.forEach(member => {
+    owner.forEach(member => {
       db.push(`guildstaff_${message.guild.id}`, member)
     });
-    db.set(`toplantirol_${message.guild.id}`, rolid)
-    const sucembed = new discord.MessageEmbed()
+    db.set(`toplantirol_${message.guild.id}`, roleid)
+    const guiltEmb = new discord.MessageEmbed()
     .setAuthor("✅ Başarılı!")
     .setDescription("**Belirttiğiniz Role Sahip Kişiler Artık Yetkili Olarak Seçildi!**")
     .setColor("GREEN")
-    return message.channel.send(sucembed)
+    return message.channel.send(guiltEmb)
 
 
 
