45c72ff790c4a81cc63b2e374785fe65e0c7bf9c
Thendra <hasan.erdem211@gmail.com>
2024-05-05 13:10:12 +0300

    Update uyarı.js

--- uyarı-sistemi/uyarı.js
+++ uyarı-sistemi/uyarı.js
@@ -2,57 +2,57 @@ const Discord = require('discord.js')
 const db = require('quick.db')
 
 exports.run = async (client, message, args) => {
-    let uarılog = await db.fetch(`ThdUyarıLog_${message.guild.id}`)
-    if(!uarılog) return message.reply(`Lütfen Bir Uyarı Log Ayarlayın Sistem Çalışmayabilir!`)
+    let warmlog = await db.fetch(`ThdUyarıLog_${message.guild.id}`)
+    if(!warmlog) return message.reply(`Lütfen Bir Uyarı Log Ayarlayın Sistem Çalışmayabilir!`)
     let user = message.mentions.members.first()  // Uyarılacak Kişi
-    let sebebmoruk = args.slice(1).join(' ') // Uyarı Sebebi
-    if(!(user||sebebmoruk)) return message.reply(`Yanlış Kullanım! Örn: \`c!uyarı @Kullanıcı [sebep]\``)
+    let reason = args.slice(1).join(' ') // Uyarı Sebebi
+    if(!(user||reason)) return message.reply(`Yanlış Kullanım! Örn: \`c!uyarı @Kullanıcı [sebep]\``)
 
-    let uyarısayı = await db.fetch(`ThdUyarıKullanıcı_${message.guild.id}_${user.id}`)
-    if(!uyarısayı) { // Kullanıcı Hiç Uyarılmadıysa
+    let warmnum = await db.fetch(`ThdUyarıKullanıcı_${message.guild.id}_${user.id}`)
+    if(!warmnum) { // Kullanıcı Hiç Uyarılmadıysa
         db.set(`ThdUyarıKullanıcı_${message.guild.id}_${user.id}`, 1) // Database'e kaydettik  **${user}** İsimli Kullanıcı ${sebebmoruk} Sebebi İle Uyarıldı!
-        let ilkuyarı = new Discord.MessageEmbed()
+        let firstWarm = new Discord.MessageEmbed()
         .setAuthor(`Thendra - Uyarı Sistemi`)
         .setColor(`WHITE`)
-        .addField("Uyarı", `\n**${user}** İsimli Kullanıcı **${sebebmoruk}** Sebebi İle Uyarıldı!\n**__Yetkili:__** **${message.author}**`)
+        .addField("Uyarı", `\n**${user}** İsimli Kullanıcı **${reason}** Sebebi İle Uyarıldı!\n**__Yetkili:__** **${message.author}**`)
         .setFooter("© 2021 Thendra", client.user.avatarURL())
         .setTimestamp()
-        message.channel.send(ilkuyarı)
+        message.channel.send(firstWarm)
         
-        let logilk = new Discord.MessageEmbed()
+        let logFirst = new Discord.MessageEmbed()
         .setAuthor(`Thendra - Uyarı Sistemi`)
         .setColor(`WHITE`)
-        .addField("Uyarı-Log", `\n**${message.author}** İsimli Yetkili **${user}** İsimli Kullanıcıyı **${sebebmoruk}** Sebebi İle Uyardı!`)
+        .addField("Uyarı-Log", `\n**${message.author}** İsimli Yetkili **${user}** İsimli Kullanıcıyı **${reason}** Sebebi İle Uyardı!`)
         .setFooter("© 2021 Thendra", client.user.avatarURL())
         .setTimestamp()
-        message.guild.channels.cache.get(uarılog).send(logilk)
+        message.guild.channels.cache.get(warmlog).send(logFirst)
     } else { // Kullanıcı Daha Önce Uyarıldıysa
         db.add(`ThdUyarıKullanıcı_${message.guild.id}_${user.id}`, 1)
-        let ikiuyarı = new Discord.MessageEmbed()
+        let secWarm = new Discord.MessageEmbed()
         .setAuthor(`Thendra - Uyarı Sistemi`)
         .setColor(`WHITE`)
-        .addField("Uyarı", `\n**${user}** İsimli Kullanıcı **${sebebmoruk}** Sebebi İle Uyarıldı!\nBu Kullanıcının Toplamda **${uyarısayı}** Uyarısı Var!\n**__Yetkili:__** **${message.author}**`)
+        .addField("Uyarı", `\n**${user}** İsimli Kullanıcı **${reason}** Sebebi İle Uyarıldı!\nBu Kullanıcının Toplamda **${warmnum}** Uyarısı Var!\n**__Yetkili:__** **${message.author}**`)
         .setFooter("© 2021 Thendra", client.user.avatarURL())
         .setTimestamp()
-        message.channel.send(ikiuyarı)
+        message.channel.send(secWarm)
 
-        let logiki = new Discord.MessageEmbed()
+        let logSec = new Discord.MessageEmbed()
         .setAuthor(`Thendra - Uyarı Sistemi`)
         .setColor(`WHITE`)
-        .addField("Uyarı-Log", `\n**${message.author}** İsimli Yetkili **${user}** İsimli Kullanıcıyı **${sebebmoruk}** Sebebi İle Uyardı!\nBu Kullanıcının Toplamda **${uyarısayı}** Uyarısı Var!`)
+        .addField("Uyarı-Log", `\n**${message.author}** İsimli Yetkili **${user}** İsimli Kullanıcıyı **${reason}** Sebebi İle Uyardı!\nBu Kullanıcının Toplamda **${warmnum}** Uyarısı Var!`)
         .setFooter("© 2021 Thendra", client.user.avatarURL())
         .setTimestamp()
-        message.guild.channels.cache.get(uarılog).send(logiki)
+        message.guild.channels.cache.get(warmlog).send(logSec)
     }
-    if(uyarısayı >= 3) {
-        user.kick(sebebmoruk)
-        let sonuyarı = new Discord.MessageEmbed()
+    if(warmnum >= 3) {
+        user.kick(reason)
+        let lastWarm = new Discord.MessageEmbed()
         .setAuthor(`Thendra - Uyarı Sistemi`)
         .setColor(`WHITE`)
         .addField("Uyarı", `\n**${user}** İsimli Kullanıcı 3 Uyarısı Olduğu İçin Sunucudan Atıldı.\n**__Yetkili:__** **${message.author}**`)
         .setFooter("© 2021 Thendra", client.user.avatarURL())
         .setTimestamp()
-        message.channel.send(sonuyarı)
+        message.channel.send(lastWarm)
         db.delete(`ThdUyarıKullanıcı_${message.guild.id}_${user.id}`)
     }
 }
@@ -60,7 +60,7 @@ exports.run = async (client, message, args) => {
 exports.conf = {
     enabled: true,
     guildOnly: true,
-    aliases: [],
+    aliases: ['warm'],
     permLevel: 2
 }
 
